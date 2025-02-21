eddd838c02b54e6e9ea48571efc00ce572681d15
NTLPY <59137305+NTLPY@users.noreply.github.com>
2025-02-21 09:59:46 +0000

    feat(p4ws/targets): Add tar command to tar/untar program package
    - Added `tar` command to tar/untar program package.
    - Added support for programs compiled by bf-p4c.

--- src/p4ws/__main__.py
+++ src/p4ws/__main__.py
@@ -5,12 +5,15 @@ import sys
 
 from . import __version__
 from .loadmn import *
+from .tar import *
 
 
 def main(args: argparse.Namespace):
     """Main of P4 Workshop executable."""
     if args.subparser_name == "loadmn":
         return main_loadmn(args)
+    elif args.subparser_name == "tar":
+        return main_tar(args)
     else:
         print(f"Unknown command: {args.subparser_name}.", file=sys.stderr)
         parser.print_help()
@@ -24,6 +27,7 @@ if __name__ == "__main__":
 
     subparsers = parser.add_subparsers(dest="subparser_name")
     make_loadmn_subparser(subparsers)
+    make_tar_subparser(subparsers)
     subparsers.add_parser("help", help="Show this help message and exit.")
     subparsers.add_parser("version", help="Show version and exit.")
 
