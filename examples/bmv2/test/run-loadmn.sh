79a4c229fc987f69bfa4490364dad4047ef31cad
NTLPY <59137305+NTLPY@users.noreply.github.com>
2025-12-02 15:52:23 +0000

    feat(examples): Add example test files for BMv2

---
+++ examples/bmv2/test/run-loadmn.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+mkdir -p logs
+
+sudo rm logs/*
+sudo rm bf_drivers.log*
+
+# Touch these two files to ensure they have the current user's access permissions.
+touch out.json shell.sh
+
+sudo -E env PYTHONPATH=${PYTHONPATH}          \
+    python3 -m p4ws loadmn                    \
+    --topo-file topo.json --net-file net.json \
+    --out-file out.json --shell-file shell.sh \
+    --log-level info
+
+rm out.json shell.sh
